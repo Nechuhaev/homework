552c49e1759a6bc2452b96a1ac205e9c1ce2ce16
Alexandr Nechuhaev <a.nechuhaev@gmail.com>
2022-09-14 21:22:10 +0300

    Pixabay

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
-const r = fetch('https://pixabay.com/api/?key=29905727-ba938f57a9499389ab5e34ef4&q=yellow+flowers&image_type=photo');
+fetch('https://pixabay.com/api/?key=29905727-ba938f57a9499389ab5e34ef4&q=yellow+flowers&image_type=photo')
+    .then(respons => {
 
-console.log(r);
+    return respons.json();
+}).then(photo => {
+    console.log(photo);
+})
+.catch(error => {
+    console.log(error);
+});
